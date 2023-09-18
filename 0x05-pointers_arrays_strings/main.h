871fade97751df35608a55da752d04e540c5db90
Lesirom <lilianesirom687@gmail.com>
2023-09-18 14:03:27 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unstdio.h>
+
+int _putchar(c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+
+end if 
